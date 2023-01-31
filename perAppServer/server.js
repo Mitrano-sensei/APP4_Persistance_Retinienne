43aac57cb22070acf3949a79a8e7fc4c4cbd28ee
Anisdhiyaeddine <dhiaeboudiaf@gmail.com>
2023-01-31 20:54:50 +0100

    imageProcessor BE added

--- perAppServer/server.js
+++ perAppServer/server.js
@@ -3,38 +3,94 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const https = require('https');
-
+const uuid = require('uuid');
+const imgProcessor = require('./imgProcessor.js');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json(
+  {
+    limit: '50mb'
+  }
+));
 
 app.get('/', (req, res) => {
-  res.status(200).send('You can post to /api/upload.');
+  res.status(200).send(
+    {
+      message: 'Welcome to APP4 Images Processing API',
+      matrix: []
+    }
+  );
 });
 
-app.post('/api/upload', (req, res) => {
-  console.log('file', req.files);
-  console.log('body', req.body);
-  res.status(200).json({
-    message: 'success!',
+app.post('/process', async (req, res) => {
+  let id = uuid.v4();
+  fs.writeFile(`tmp/${id}.png`, req.body.source, 'base64', (err) => {
+    if (err) {
+      console.log(err);
+    }
   });
+
+  let config = req.body.config;
+  let matrix = null;
+  try {
+    if (Object.keys(config.resize).length > 0) {
+      await imgProcessor.resizeImage(`tmp/${id}.png`, `tmp/${id}.png`, config.resize.width, config.resize.height);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 50));  
+    if (config.contrast >= -1 && config.contrast <= 1) {
+      await imgProcessor.setImageContrast(`tmp/${id}.png`, `tmp/${id}.png`, config.contrast);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 50));  
+    if (config.brightness >= 0) {
+      await imgProcessor.setImageBrightness(`tmp/${id}.png`, `tmp/${id}.png`, config.brightness);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 50));  
+    if (config.saturation >= 0) {
+      await imgProcessor.setImageSaturation(`tmp/${id}.png`, `tmp/${id}.png`, config.saturation);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 50));  
+    if (config.toMatrix) {
+      matrix = await imgProcessor.getMatrixFromImage(`tmp/${id}.png`);
+      }
+  } catch (err) {
+    console.log(err);
+  }
+
+  // return either matrix or image
+  let image = null;
+  if (!config.toMatrix) {
+    fs.readFile(`tmp/${id}.png`, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      image = data;
+    }); 
+  }
+
+  await new Promise(resolve => setTimeout(resolve, 50));
+  res.status(200).send(
+    {
+      message: 'Image processed successfully', 
+      payload: config.toMatrix ? matrix : image
+    }
+  );
+  
+  // Delete tmp/img and replace it with processed img 
+
+  
+    
+
+
+    
 });
 
-// app.listen(process.env.PORT || 3000, () => {
-//   console.log(
-//     `server is running at http://localhost:${process.env.PORT || 3000}`
-//   );
-// });
 
 
-const options = {
-  key: fs.readFileSync('privatekey.pem'),
-  cert: fs.readFileSync('certificate.pem')
-};
-
-https.createServer(options, app).listen(3000, () => {
-  console.log('server is running at https://localhost:3000');
+app.listen(5000, () => {
+  console.log('server is running at http://localhost:5000');
 });
-
-
